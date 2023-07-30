e242df63b64287f0429048c73f798ad1165e5e27
hanmok <dmammmm@naver.com>
2023-07-30 12:28:41 +0900

    Add modules, services, controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { GenreModule } from './genre/genre.module';
+import { QuestionModule } from './question/question.module';
+import { SectionModule } from './section/section.module';
+import { SurveyModule } from './survey/survey.module';
+import { ResponseModule } from './response/response.module';
+import { SegmentModule } from './segment/segment.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, GenreModule, QuestionModule, SectionModule, SurveyModule, ResponseModule, SegmentModule],
   controllers: [AppController],
   providers: [AppService],
 })
