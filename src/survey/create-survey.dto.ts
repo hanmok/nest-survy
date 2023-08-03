ebeed50fdc3a539f2c020385c84728c92efbd5a9
hanmok <dmammmm@naver.com>
2023-08-03 16:21:00 +0900

    modify createSurveyDto

--- src/survey/create-survey.dto.ts
+++ src/survey/create-survey.dto.ts
@@ -1,13 +1,10 @@
-import { Expose } from "class-transformer";
 import { IsNumber, IsString } from "class-validator";
 
 
 export class CreateSurveyDTO { 
-	@Expose()
 	@IsString()
 	title: string;
 
-	@Expose()
 	@IsNumber()
 	participationGoal: number;
 }
