eabbf1841f1f773734f54470fd67b15ff6ae4b66
hanmok <dmammmm@naver.com>
2023-10-09 20:32:36 +0900

    working fine..

--- src/gql/db/participatings.ts
+++ src/gql/db/participatings.ts
@@ -4,6 +4,7 @@
 import { Participating } from 'src/participating/participating.entity';
 import { connection } from './connection';
 import { getMatchedSurveys } from './surveys';
+import logObject from 'src/util/logObject';
 
 // 이전: 2afe386a37ef6b5448657983612ff05b8781df90
 // 이후: 3c1f102ddfc29a54f2c203cd793c9c4358a889f5
@@ -16,8 +17,9 @@ export async function getParticipatedSurveys() {
 
 export async function getParticipatedSurveysByUserId(user_id: number) {
   const matchedSurveyIds = (await getParticipatingTable())
-    .filter((participating) => participating.user_id === user_id)
+    // .filter((participating) => participating.user_id === user_id)
     .map((participating) => participating.survey_id);
+  logObject('matchedSurveyIds', matchedSurveyIds);
   const matchedSurveys = await getMatchedSurveys(matchedSurveyIds);
   return matchedSurveys;
 }
